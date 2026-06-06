21dfd774ed60b7adbfebdbba98033a3b1d3c9b78
Darpan <imposterrr77@gmail.com>
2026-06-06 17:17:22 +0530

    initial weather app

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log(API_KEY);
+const result = document.getElementById("result");
+const cityInput = document.getElementById("cityInput");
+const searchBtn = document.getElementById("searchBtn");
+
+function getWeather() {
+    const city = cityInput.value.trim();
+
+    if (city === "") {
+        result.innerHTML = "<p>Please enter a city name</p>";
+        return;
+    }
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+
+            // check API response
+            if (data.cod !== 200) {
+                result.innerHTML = "<p>City not found</p>";
+                return;
+            }
+
+            result.innerHTML = `
+                <h2>${data.name}</h2>
+                <p>🌡️ Temp: ${data.main.temp}°C</p>
+                <p>💧 Humidity: ${data.main.humidity}%</p>
+                <p>☁️ Weather: ${data.weather[0].description}</p>
+            `;
+        })
+        .catch(error => {
+            result.innerHTML = "<p>Something went wrong</p>";
+            console.log(error);
+        });
+}
+
+searchBtn.addEventListener("click", getWeather);
